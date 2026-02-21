24a974d13ee770e564c18cd5d44fcc4c09ee5743
Nadav Shohat <nadav@projectcircle.co.il>
2026-02-21 09:33:35 +0200

    Remove ECR from module, pin provider versions

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 1.3"
+  required_version = ">= 1.11.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "~> 6.33"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.20"
+      version = "~> 3.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.10"
+      version = "~> 3.1"
     }
   }
 }
